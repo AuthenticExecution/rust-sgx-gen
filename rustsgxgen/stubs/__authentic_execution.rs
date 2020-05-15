5417656074ef76207daf57d93a2ed247610895c2
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-05-15 10:26:02 +0200

    refactor "ae_crypto" to "reactive_crypto"

--- rustsgxgen/stubs/__authentic_execution.rs
+++ rustsgxgen/stubs/__authentic_execution.rs
@@ -1,6 +1,6 @@
 pub mod authentic_execution {
     extern crate base64;
-    extern crate ae_crypto;
+    extern crate reactive_crypto;
     extern crate reactive_net;
 
     use std::collections::HashMap;
@@ -9,11 +9,11 @@ pub mod authentic_execution {
     use std::net::TcpStream;
 
     use reactive_net::{ResultCode, CommandCode, ResultMessage, CommandMessage};
-    use ae_crypto::Encryption;
+    use reactive_crypto::Encryption;
     use crate::__run::MODULE_KEY;
 
     mod connection {
-        use ae_crypto::Encryption;
+        use reactive_crypto::Encryption;
 
         pub struct Connection {
             index : u16,
@@ -119,7 +119,7 @@ pub mod authentic_execution {
             Err(_)  => return failure(ResultCode::InternalError, None)
         };
 
-        let key = match ae_crypto::decrypt(cipher, &decoded_key, data_to_u16(nonce), &ad, &Encryption::Aes) {
+        let key = match reactive_crypto::decrypt(cipher, &decoded_key, data_to_u16(nonce), &ad, &Encryption::Aes) {
            Ok(k)    => k,
            Err(_)   => return failure(ResultCode::CryptoError, None)
         };
@@ -156,7 +156,7 @@ pub mod authentic_execution {
         };
 
         let nonce = conn.get_nonce();
-        let data = match ae_crypto::decrypt(payload, conn.get_key(), nonce, &u16_to_data(nonce), conn.get_encryption()) {
+        let data = match reactive_crypto::decrypt(payload, conn.get_key(), nonce, &u16_to_data(nonce), conn.get_encryption()) {
            Ok(d) => d,
            Err(_) => return failure(ResultCode::CryptoError, None)
         };
@@ -186,7 +186,7 @@ pub mod authentic_execution {
         };
 
         let nonce = conn.get_nonce();
-        let payload = match ae_crypto::encrypt(data, conn.get_key(), nonce, &u16_to_data(nonce), conn.get_encryption()) {
+        let payload = match reactive_crypto::encrypt(data, conn.get_key(), nonce, &u16_to_data(nonce), conn.get_encryption()) {
            Ok(p) => p,
            Err(e) => {
                debug(&format!("{}", e));
